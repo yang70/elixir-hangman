f53baabed5e13693d7a01de97c08414e45665382
Matthew Yang <yang70@gmail.com>
2019-01-25 15:07:29 -0800

    score a good move

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -7,10 +7,14 @@ defmodule Hangman.Game do
     used:       MapSet.new()
   )
   
-  def new_game() do
+  def new_game(word) do
     %Hangman.Game{ 
-      letters: Dictionary.random_word |> String.codepoints 
+      letters: word |> String.codepoints 
     }
+  end
+  
+  def new_game() do
+    new_game(Dictionary.random_word)
   end
   
   def make_move(game = %{ game_state: state }, _guess) when state in [ :won, :lost ] do
@@ -27,11 +31,28 @@ defmodule Hangman.Game do
   end
   
   def accept_move(game, guess, _already_guessed) do
-    Map.put(game, :used, MapSet.put(game.used, guess))
+    Map.put( game, :used, MapSet.put(game.used, guess) )
+    |> score_guess( Enum.member?(game.letters, guess) )
+  end
+  
+  def score_guess(game, _good_guess = true) do
+    new_state = MapSet.new(game.letters)
+    |> MapSet.subset?(game.used)
+    |> maybe_won()
+    Map.put(game, :game_state, new_state)
+  end
+  
+  def score_guess(game, _not_good_guess) do
+  #   dec turns left
+  #   0? :lost, :bad_guess
+    game
   end
   
   def tally(_game) do
     123
   end
+  
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
   
 end
